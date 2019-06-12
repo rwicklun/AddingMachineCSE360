5a282eee84e1a5735b6f4b7d2df59815dc8949a2
Richard Wicklund <rwicklun@asu.edu>
2019-06-11 19:17:31 -0700

    This is the initial commit of AddingMachine.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+/**
+ * <h1>Adding Machine</h1>
+ * The program AddingMachine currently sets a variable to 0,
+ * and returns 0, there are no other functionalities.
+ * <p>
+ * @author Richard Wicklund
+ * @version Assignment 2: 1.0
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This is the constructor for AddingMachine, sets the variable total to 0.
+	 * <p> 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The getTotal method returns 0 when called.
+	 * @return This returns a value of 0.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The add method is an empty method it does nothing, and takes in an integer input.
+	 * @param value This is the input value when the add method is called.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * The subtract method is an empty method it does nothing, and takes in an integer input.
+	 * @param value This is the input value when the subtract method is called.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * The toString method returns an empty String when called.
+	 * @return This returns an empty String.
+	 */
+	public String toString () {
+		return "";
+	}
+	/**
+	 * The clear method is an empty method, it does nothing,
+	 * has no inputs, and returns nothing.
+	 */
+	public void clear() {
+	
+	}
+}
